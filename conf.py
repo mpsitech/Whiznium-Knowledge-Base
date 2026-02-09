831903daf363ba07f42871f535dba5702ac07ce6
Alexander Wirthmueller <aw@mpsitech.com>
2026-02-10 03:09:17 +0700

    full display width

--- conf.py
+++ conf.py
@@ -27,3 +27,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {"collapse_navigation": False}
 html_static_path = ['_static']
+html_css_files = ['custom.css']
