95cca1f04851a1fe6f7d09891e17f371448648c0
Alexander Wirthmueller <aw@mpsitech.com>
2026-02-11 11:58:14 +0700

    retain referenced images in deploy

--- remake.sh
+++ remake.sh
@@ -1,5 +1,10 @@
 make clean
+
 make html
+for kb_dir in "$PWD"/KB*; do
+	if [ -d "$kb_dir" ]; then
+		kb_name=$(basename "$kb_dir")
+		cp -r "$kb_dir" "$PWD/_build/html/"
+	fi
+done
 touch _build/html/.nojekyll
-
-
